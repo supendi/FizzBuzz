d65bc0644dd101cd4cf4786f5368f45221e5250a
supendi <irpan.supendi@gmail.com>
2022-05-20 21:22:02 +0700

    init

---
+++ FizzBuzz.ConsoleApp/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.ConsoleApp
+{
+    public class FizzBuzz
+    {
+        /// <summary>
+        /// Print fizz, buzz or fizzbuzz
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static string Print(int i)
+        {
+            var isFizzBuzz = i % 3 == 0 && i % 5 == 0;
+            var isBuzz = i % 5 == 0;
+            var isFizz = i % 3 == 0;
+
+            var text = isFizzBuzz ? "FizzBuzz" : isBuzz ? "Buzz" : isFizz ? "Fizz" : i.ToString();
+            return text;
+        }
+    }
+}
---
+++ FizzBuzz.ConsoleAppTests/FizzBuzzTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.ConsoleApp.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+        [TestMethod()]
+        public void PrintTest1_PrintNumber()
+        {
+            var input = 1;
+            var actual = FizzBuzz.Print(input);
+            var expected = input.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PrintTest2_PrintFizz()
+        {
+            var input = 3;
+            var actual = FizzBuzz.Print(input);
+            var expected = "Fizz";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PrintTest3_PrintBuzz()
+        {
+            var input = 5;
+            var actual = FizzBuzz.Print(input);
+            var expected = "Buzz";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PrintTest4_PrintFizzBuzz()
+        {
+            var input = 15;
+            var actual = FizzBuzz.Print(input);
+            var expected = "FizzBuzz";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
